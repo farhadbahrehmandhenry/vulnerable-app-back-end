8a889724bf899463cd7e4cfe8cb24fbe4bf89eb6
Farhad <fbahrehmand82@gmail.com>
2020-11-27 13:56:52 -0800

    os

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const db = require('../db');
 const _ = require('lodash');
 const router = express.Router();
+const {exec} = require('child_process');
+
 
 router.post('/bad/sql', async (req, res, next) => {
   try {
@@ -28,5 +30,59 @@ router.post('/good/sql', async (req, res, next) => {
     res.sendStatus(500);
   }
 });
+
+// os injection
+router.post('/bad/os/injection', async (req, res, next) => {
+  var start = new Date().getTime();
+
+  try {
+    exec(`touch ${req.body.fileName}`, (error, stdout, stderr) => {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        else {
+          var end = new Date().getTime();
+
+          res.json({type: 'success', fileName: req.body.fileName, time: end - start});
+        }
+    });
+  }
+  catch(e) {
+    console.log(e)
+
+    res.sendStatus(500);
+  }
+});
+
+router.post('/good/os/injection', async (req, res, next) => {
+  var start, end;
+  
+  start = new Date().getTime();
+
+  try {
+    exec(`touch ${req.body.fileName}`, (error, stdout, stderr) => {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        else {
+          end = new Date().getTime();
+
+          res.json({type: 'success', fileName: req.body.fileName});
+        }
+    });
+  }
+  catch(e) {
+    console.log(e)
+
+    res.sendStatus(500);
+  }
+
+  console.log(start, end)
+
+});
+
+
+
+
 
 module.exports = router;
